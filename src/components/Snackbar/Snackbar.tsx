9f1fb512958cc1a716299be0ad1821af0b035e65
Keila <keila.amada@truckpag.com.br>
2024-12-07 11:40:27 -0300

    feat: new componente for snackbar

---
+++ src/components/Snackbar/Snackbar.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+type SnackbarProps = {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  message: string;
+  severity: "success" | "error" | "warning" | "info";
+};
+
+export default function SimpleSnackbar({
+  setIsOpen,
+  isOpen,
+  message,
+  severity,
+}: SnackbarProps) {
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setIsOpen(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <Button color="inherit" size="small" onClick={handleClose}>
+        Close
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="default"
+        onClick={handleClose}
+      ></IconButton>
+    </React.Fragment>
+  );
+
+  return (
+    <Snackbar
+      open={isOpen}
+      onClose={handleClose}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      autoHideDuration={3000}
+    >
+      <Alert severity={severity} action={action}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+}
