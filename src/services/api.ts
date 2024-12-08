ac87ff4e85e71a11ac76a993ece684ea10287ebe
Keila <keila.amada@truckpag.com.br>
2024-12-08 00:01:04 -0300

    refactor: enviroment variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL, // Usando a variável de ambiente para a base URL
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
   auth: {
-    username: process.env.REACT_APP_API_USERNAME || "", // Usando a variável de ambiente para o nome de usuário
-    password: process.env.REACT_APP_API_PASSWORD || "", // Usando a variável de ambiente para a senha
+    username: process.env.REACT_APP_API_USERNAME || "psAdmin",
+    password: process.env.REACT_APP_API_PASSWORD || "goledger",
   },
 });
 
