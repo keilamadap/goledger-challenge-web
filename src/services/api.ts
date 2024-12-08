689691ec6397c051168c79ccccbe2693aac731d7
Keila <keila.amada@truckpag.com.br>
2024-12-08 00:03:59 -0300

    refactor: enviroment variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: "http://ec2-54-91-215-149.compute-1.amazonaws.com/api",
   headers: {
     "Content-Type": "application/json",
   },
   auth: {
-    username: process.env.REACT_APP_API_USERNAME || "psAdmin",
-    password: process.env.REACT_APP_API_PASSWORD || "goledger",
+    username: "psAdmin",
+    password: "goledger",
   },
 });
 
