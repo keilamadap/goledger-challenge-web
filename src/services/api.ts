6db07fb83d4e25b02c9d290b90e917b5a6483a9a
Keila <keila.amada@truckpag.com.br>
2024-12-09 17:03:59 -0300

    refactor: adding fallback to api settings

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL:
+    process.env.REACT_APP_API_BASE_URL ||
+    "http://ec2-54-91-215-149.compute-1.amazonaws.com/api",
   headers: {
     "Content-Type": "application/json",
   },
   auth: {
-    username: process.env.REACT_APP_API_USERNAME || "",
-    password: process.env.REACT_APP_API_PASSWORD || "",
+    username: process.env.REACT_APP_API_USERNAME || "psAdmin",
+    password: process.env.REACT_APP_API_PASSWORD || "goledger",
   },
 });
-
-// console.log("Base URL:", process.env.REACT_APP_API_BASE_URL);
 
 export default api;
